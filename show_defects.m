4bcc58d650917a8cde2ffc13f595635c1643360b
MarcoCastelli4 <36838250+MarcoCastelli4@users.noreply.github.com>
2022-01-29 16:08:54 +0100

    inserito imcrop

--- show_defects.m
+++ show_defects.m
@@ -1,7 +1,12 @@
-function show_defects(img,mask,kernel_size,subplotplace)
+function show_defects(img,mask,kernel_size,subplotplace,type)
 A=img;
-A = A(ceil(kernel_size/2):end-floor(kernel_size/2),ceil(kernel_size/2):end-floor(kernel_size/2)); %499x499
-
+if(not(strcmp(type,'crop')))
+    A = A(ceil(kernel_size/2):end-floor(kernel_size/2),ceil(kernel_size/2):end-floor(kernel_size/2)); %499x499
+else
+    R=kernel_size(1,1);
+    C=kernel_size(1,2);
+    A = A(ceil(R/2):end-floor(R/2),ceil(C/2):end-floor(C/2));
+end
 A1 = A;
 A1(mask)=255;
 
@@ -10,5 +15,6 @@ Af = cat(3,A1,A,A);
 subplot(subplotplace)
 imshowpair(A,Af,'montage')
 title ('Immagine e Difetto finale')
+
 end
 
