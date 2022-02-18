1f709783b517b6cce5a777d429ce2d69072eda43
MarcoCastelli4 <36838250+MarcoCastelli4@users.noreply.github.com>
2022-02-18 09:50:50 +0100

    Clean some mask

---
+++ cleanSomeMask.m
@@ -0,0 +1,11 @@
+function [newMask] = cleanSomeMask(mask,mean,number_of_mask)
+
+j=1;    %indice per nuova maschera
+for i=1:number_of_mask
+    s=sum(mask{i},'all');
+    if(s>mean/2 && s<mean*2)  %le maschere valide devono stare tra la metà e il valore doppio
+        newMask{j}=mask{i}
+        j=j+1;
+    end       
+end
+end
